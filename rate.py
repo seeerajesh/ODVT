1d3b4433e766cf5ec859bd4787bacd3b78dfbee4
seeerajesh <seeerajesh@gmail.com>
2025-02-13 00:44:46 +0530

    Update rate.py

--- rate.py
+++ rate.py
@@ -1,6 +1,6 @@
 import json
 
-with open("solid-scheme-450717-q8-f5d059394adf.json", "r") as file:
+with open("/Users/admin/Downloads/solid-scheme-450717-q8-f5d059394adf.json", "r") as file:
     creds = json.load(file)
 
 print(json.dumps(creds))  # Copy this output for GitHub Secret
